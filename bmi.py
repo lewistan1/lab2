eb964f50d367c1d9881bc51a0f2a89a69230befa
lewisTan <lewis.23@ichat.sp.edu.sg>
2024-04-29 16:39:26 +0800

    new bmi

--- bmi.py
+++ bmi.py
@@ -3,14 +3,12 @@ def calculate_bmi(height, weight):
     print("Weight = " + str(weight))
     Bmi=weight/(height**2)
     if Bmi < 18.5:
-        classification = "Under Weight"
+        print("Under Weight")
         return -1
     elif 18.5 <= Bmi <= 25.0:
-        classification = "Normal Weight"
+        print("Normal Weight")
         return 0
     else:
-        classification = "Over Weight"
+        print("Over Weight")
         return 1
-    print("BMI = {:.2f}".format(Bmi))
-    print("Weight Classification: " + classification)
 calculate_bmi(weight=57, height=1.73) 
