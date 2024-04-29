e64ad371e7da241ae95e15b14b6bb4bb2597d0e0
lewisTan <lewis.23@ichat.sp.edu.sg>
2024-04-29 15:57:24 +0800

    it correct

--- bmi.py
+++ bmi.py
@@ -2,12 +2,15 @@ def calculate_bmi(height, weight):
     print("Height = " + str(height))
     print("Weight = " + str(weight))
     Bmi=weight/(height**2)
-    if Bmi < -1:
+    if Bmi < 18.5:
         classification = "Under Weight"
-    elif Bmi==0:
+        return -1
+    elif 18.5 <= Bmi <= 25.0:
         classification = "Normal Weight"
+        return 0
     else:
         classification = "Over Weight"
+        return 1
     print("BMI = {:.2f}".format(Bmi))
     print("Weight Classification: " + classification)
 calculate_bmi(weight=57, height=1.73) 
